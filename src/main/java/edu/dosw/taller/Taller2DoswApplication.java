4e96540c84a325a08a619bc04f867182ae87b98b
NikoMAR3 <nikolas.martinezrive@mail.escuelaing.edu.co>
2025-09-24 08:59:42 -0500

    scaffolding y add de dependecias en el pom

---
+++ src/main/java/edu/dosw/taller/Taller2DoswApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.taller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2DoswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller2DoswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/taller/Taller2DoswApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller2DoswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
